235e603bc9d2d65064d950cd76d3aea995776715
varun <varunkr2209@gmail.com>
2025-04-09 23:26:24 +0530

    Updates

---
+++ precache-manifest.cdddf52a86aa64bbe9e7d48d87812b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8757d84f606f03457835a6237443e64",
+    "url": "/Atria-Navigation/index.html"
+  },
+  {
+    "revision": "62b0840fa9cb9b07bb70",
+    "url": "/Atria-Navigation/static/css/main.35bb4fb1.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/Atria-Navigation/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Atria-Navigation/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "62b0840fa9cb9b07bb70",
+    "url": "/Atria-Navigation/static/js/main.94a97cd1.chunk.js"
+  },
+  {
+    "revision": "2f2d626b1b39d747069c",
+    "url": "/Atria-Navigation/static/js/runtime~main.5ea20621.js"
+  }
+]);
